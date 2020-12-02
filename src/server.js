de326e45f2c37f89ecf978c9293993ec9daf7bad
João Vitor Dias <jvitordiass@outlook.com>
2020-12-02 14:55:16 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
+import dotenv from 'dotenv'
 
 import routes from './routes.js'
 
@@ -8,8 +9,10 @@ const APP_PORT = process.env.PORT || 3001
 
 const app = express()
 
+dotenv.config()
+
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join('client/build')))
 app.use(routes)
 app.listen(APP_PORT, () => console.log('> Server is running...'))
